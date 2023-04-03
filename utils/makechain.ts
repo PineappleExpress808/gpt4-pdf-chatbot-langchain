f9cd8155b73a3d4178644af0ef3e15e1da46d0f8
rlm <31treehaus@31s-MacBook-Pro.local>
2023-04-03 15:36:04 -0700

    test for travel app

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -13,10 +13,7 @@ Follow Up Input: {question}
 Standalone question:`);
 
 const QA_PROMPT = PromptTemplate.fromTemplate(
-  `You are an AI assistant providing helpful advice. You are given the following extracted parts of a long document and a question. Provide a conversational answer based on the context provided.
-You should only provide hyperlinks that reference the context below. Do NOT make up hyperlinks.
-If you can't find the answer in the context below, just say "Hmm, I'm not sure." Don't try to make up an answer.
-If the question is not related to the context, politely respond that you are tuned to only answer questions that are related to the context.
+  `You are a travel agent that makes reccomendations about group travel booking. The following pieces of context are vendors that you should suggest to the user in response to questions. But, also draw on your general world knowledge and common sense when suggesting vendors to the user.
 
 Question: {question}
 =========
@@ -36,7 +33,7 @@ export const makeChain = (
   const docChain = loadQAChain(
     new OpenAIChat({
       temperature: 0,
-      modelName: 'gpt-4', //change this to older versions (e.g. gpt-3.5-turbo) if you don't have access to gpt-4
+      modelName: 'gpt-3.5-turbo', //change this to older versions (e.g. gpt-3.5-turbo) if you don't have access to gpt-4
       streaming: Boolean(onTokenStream),
       callbackManager: onTokenStream
         ? CallbackManager.fromHandlers({
